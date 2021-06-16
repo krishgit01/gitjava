6b5b4ff2dad3cbc1c45707c23ca7329dd4d55fc6
krishgit01 <75493203+krishgit01@users.noreply.github.com>
2021-06-16 10:22:38 +0530

    Update b2.java
    pull ex line added

--- TestJava/src/b2.java
+++ TestJava/src/b2.java
@@ -2,6 +2,8 @@ public class b2 {
     public static void main (String[] args){
         System.out.println ("I am created in branch2");
         //I am added by second user
+       // i am added by krishna locally in branch2
+       //pull ex asked by team
         // i am added by krishna locally in branch2
     }
 }
