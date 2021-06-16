5306e76ed58e6e38957e061f854dd67434203ae6
kris <krishnaprakashm@yahoo.com>
2021-06-16 10:16:55 +0530

    updated b2.java by krish

--- TestJava/src/b2.java
+++ TestJava/src/b2.java
@@ -1,5 +1,6 @@
 public class b2 {
     public static void main (String[] args){
         System.out.println ("I am created in branch2");
+        // i am added by krishna locally in branch2
     }
 }
